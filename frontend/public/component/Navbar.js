2ef029632049973db3bb7fb33b737e2b89d5aed2
q.lescut <q.lescut@gmail.com>
2022-01-12 17:03:22 +0100

    create header

--- frontend/public/component/Navbar.js
+++ frontend/public/component/Navbar.js
@@ -4,7 +4,9 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
     return (
         <nav>
-            <Link to="/Index"><img src=""/></Link>
+            <Link to="/Index"><img src="" alt=""/></Link>
+            <button><img src="" alt=""/></button>
+            <button><img src="" alt=""/></button>
         </nav>
     )
 }
